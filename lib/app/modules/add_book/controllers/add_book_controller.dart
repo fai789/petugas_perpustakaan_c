e0a52bf9fb31a255a96bbb85f66c316bb52bc087
fai789 <rifai.l168ll@gmail.com>
2024-01-30 15:18:02 +0700

    p

---
+++ lib/app/modules/add_book/controllers/add_book_controller.dart
@@ -0,0 +1,73 @@
+import 'dart:developer';
+import 'package:dio/dio.dart' as dio;
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:petugas_perpustakaan_c/app/data/constan/endpoin.dart';
+import 'package:petugas_perpustakaan_c/app/data/provider/api_provider.dart';
+import '../../../data/provider/storage_provider.dart';
+import '../../../routes/app_pages.dart';
+
+class AddBookController extends GetxController {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController judulController = TextEditingController();
+  final TextEditingController penulisController = TextEditingController();
+  final TextEditingController penerbitController = TextEditingController();
+  final TextEditingController tahun_terbitController = TextEditingController();
+
+  //TODO: Implement LoginController
+
+  final loading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  AddBook() async {
+    loading(true);
+    try {
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.book,
+            data: {
+              "judul": judulController.text.toString(),
+              "penulis": penulisController.text.toString(),
+              "penerbit": penerbitController.text.toString(),
+              "tahun_terbit": int.parse(tahun_terbitController.text.toString()),
+            });
+
+        if (response.statusCode == 201) {
+          Get.snackbar("Sukses", "Input Berhasil", backgroundColor: Colors.green);
+          Get.toNamed(Routes.BOOK);
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loading(false);
+    } on DioException catch (e) {
+      loading(false);
+      if (e.response != null) {
+        if (e.response?.data != null)
+          Get.snackbar("sorry", "${e.response?.data['message']}",
+              backgroundColor: Colors.orange);
+      } else {
+        Get.snackbar("sorry", e.message ?? "", backgroundColor: Colors.red);
+      }
+    } catch (e) {
+      loading(false);
+      Get.snackbar("error", e.toString(), backgroundColor: Colors.red);
+    }
+  }
+}
---
+++ lib/app/modules/add_book/views/add_book_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/add_book_controller.dart';
+
+
+class AddBookView extends GetView<AddBookController> {
+  const AddBookView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AddBookView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Form(
+            key: controller.formKey,
+            child: Column(children: [
+              TextFormField(
+                  controller: controller.judulController,
+                  decoration: InputDecoration(hintText: "Masukan Judul"),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                  controller: controller.penulisController,
+                  decoration: InputDecoration(hintText: "Masukan Penulis"),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Password tidak boleh kosong";
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                  controller: controller.penerbitController,
+                  decoration: InputDecoration(hintText: "Masukan Penerbit"),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                  controller: controller.tahun_terbitController,
+                  decoration: InputDecoration(hintText: "Masukan Tahun Terbit"),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  }),
+              Obx(() => controller.loading.value
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                  onPressed: () {
+                    controller.AddBook();
+                  },
+                  child: Text("Add Book")))
+            ])),
+      ),
+    );
+  }
+}
